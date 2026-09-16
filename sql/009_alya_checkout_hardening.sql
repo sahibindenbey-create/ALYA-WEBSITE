3914e92950e5cad32bd7882dd3abdf6193053a4b
sahibindenbey-create <sahibindenbey@gmail.com>
2026-09-17 01:56:54 +0300

    fix: make checkout hardening migration SQL Server safe

--- sql/009_alya_checkout_hardening.sql
+++ sql/009_alya_checkout_hardening.sql
@@ -7,7 +7,20 @@ BEGIN
     ALTER TABLE dbo.AlyaOrders ADD PaymentReference NVARCHAR(120) NULL;
   IF COL_LENGTH('dbo.AlyaOrders','CheckoutToken') IS NULL
     ALTER TABLE dbo.AlyaOrders ADD CheckoutToken NVARCHAR(120) NULL;
-
-  IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name='UX_AlyaOrders_CheckoutToken' AND object_id=OBJECT_ID('dbo.AlyaOrders'))
-    CREATE UNIQUE INDEX UX_AlyaOrders_CheckoutToken ON dbo.AlyaOrders(CheckoutToken) WHERE CheckoutToken IS NOT NULL;
 END;
+GO
+
+IF OBJECT_ID('dbo.AlyaOrders','U') IS NOT NULL
+BEGIN
+  IF COL_LENGTH('dbo.AlyaOrders','CheckoutToken') IS NOT NULL
+    AND NOT EXISTS (
+      SELECT 1
+      FROM sys.indexes
+      WHERE name='UX_AlyaOrders_CheckoutToken'
+        AND object_id=OBJECT_ID('dbo.AlyaOrders')
+    )
+    CREATE UNIQUE INDEX UX_AlyaOrders_CheckoutToken
+      ON dbo.AlyaOrders(CheckoutToken)
+      WHERE CheckoutToken IS NOT NULL;
+END;
+GO
