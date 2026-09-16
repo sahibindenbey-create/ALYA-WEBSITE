925d70c2c4bba798d90bfa995ef044122a1d6921
sahibindenbey-create <sahibindenbey@gmail.com>
2026-09-16 11:49:39 +0300

    design: load refreshed ALYA HOMES storefront styles

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import './alyahomes-reference.css';
 import { StoreProvider } from './store';
 export const metadata={title:'ALYA HOMES | Ev Yaşam Ürünleri',description:'ALYA HOMES ürün koleksiyonu'};
 export default function RootLayout({children}){return <html lang="tr"><body><StoreProvider>{children}</StoreProvider></body></html>}
