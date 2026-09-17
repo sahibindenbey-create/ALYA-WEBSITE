ce47e4ebcfdd5677dc60bf3d4f8edf5e7ab03909
sahibindenbey-create <sahibindenbey@gmail.com>
2026-09-17 16:58:26 +0300

    feat: add ALYA HOMES company information

---
+++ app/company-info.js
@@ -0,0 +1,10 @@
+export const COMPANY={
+ name:'ALYA HOMES DAYANIKLI TÜKETİM MALLARI SANAYİ TİCARET LİMİTED ŞİRKETİ',
+ shortName:'ALYA HOMES',
+ address:'Cevizli Mah. Tugay Yolu Cad. Maltepe Park AVM No:67/B4-18/19 Maltepe / İSTANBUL',
+ phone:'0216 594 78 60',
+ email:'info@alyahomes.com.tr',
+ taxOffice:'KARTAL',
+ taxNumber:'0680886722',
+ mersisNumber:'0068088672200018'
+};
