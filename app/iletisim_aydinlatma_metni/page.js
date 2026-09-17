3f7ab4b975ef8bc699bcd40a5965034add13998a
sahibindenbey-create <sahibindenbey@gmail.com>
2026-09-17 16:56:25 +0300

    feat: add communication disclosure page

---
+++ app/iletisim_aydinlatma_metni/page.js
@@ -0,0 +1,3 @@
+import LegalPage from '../legal/LegalPage';
+export const metadata={title:'İletişim Aydınlatma Metni | ALYA HOMES'};
+export default function Page(){return <LegalPage type="communication"/>}
