4cc8a94ffc9b46e9ba1c346a29399e0cbf5f3960
sahibindenbey-create <sahibindenbey@gmail.com>
2026-09-17 16:56:57 +0300

    feat: add security certificates page

---
+++ app/guvenlik-sertifikalari/page.js
@@ -0,0 +1,3 @@
+import LegalPage from '../legal/LegalPage';
+export const metadata={title:'Güvenlik Sertifikaları | ALYA HOMES'};
+export default function Page(){return <LegalPage type="security"/>}
