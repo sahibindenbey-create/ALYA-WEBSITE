53766bb9319ef7da6cfdbb557adcb72905856a74
sahibindenbey-create <sahibindenbey@gmail.com>
2026-09-17 16:56:06 +0300

    feat: add privacy page

---
+++ app/kisisel_verilerin_korunmasi/page.js
@@ -0,0 +1,3 @@
+import LegalPage from '../legal/LegalPage';
+export const metadata={title:'Kişisel Verilerin Korunması | ALYA HOMES'};
+export default function Page(){return <LegalPage type="privacy"/>}
